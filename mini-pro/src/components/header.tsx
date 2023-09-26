03162d3300436c2d1e4e0754b72080bc6ffdd157
iceqi <qqwei1123@163.com>
2023-09-26 17:33:56 +0800

    add header

--- mini-pro/src/components/header.tsx
+++ mini-pro/src/components/header.tsx
@@ -1,28 +1,24 @@
 import React from "react";
 import Taro from "@tarojs/taro";
-import { View, Text } from "@tarojs/components";
+import { View} from "@tarojs/components";
+import { AtNavBar} from "taro-ui";
 
-import  "./header.scss"
 function CustomHeader({ title }) {
-    const handleGoBack = () => {
-        Taro.navigateBack();
-    };
 
-    const handleGoToHome = () => {
-        Taro.redirectTo({ url: "pages/index/index" });
-    };
 
-    return (
-        <View className="custom-header">
-            <View className="header-left" onClick={handleGoBack}>
-                <Text>&lt; Back</Text>
-            </View>
-            <View className="header-title">{title}</View>
-            <View className="header-right" onClick={handleGoToHome}>
-                <Text>Home</Text>
-            </View>
-        </View>
-    );
+  const handleGoToHome = () => {
+    Taro.redirectTo({ url: "pages/index/index" });
+  };
+
+  return (
+    <View className="custom-header">
+      <AtNavBar
+        title={title}
+        rightFirstIconType="home"
+        onClickRgIconSt={handleGoToHome} // Use the same function for both buttons
+      />
+    </View>
+  );
 }
 
 export default CustomHeader;
--- mini-pro/src/pages/index/index.tsx
+++ mini-pro/src/pages/index/index.tsx
@@ -5,34 +5,36 @@ import CustomHeader from "@/components/header";
 import { AtGrid } from "taro-ui";
 
 export default function Index() {
-    useLoad(() => {
-        console.log("Page loaded.");
-    });
-    const handleGridItemClick = (item, index) => {
-        console.log(`Clicked on item ${index + 1}: ${item.link}`);
-        // Add your custom logic here
-        Taro.redirectTo({ url: item.link });
-    };
-    return (
-        <View className="index">
-            <CustomHeader title="Home" />
-            <AtGrid
-                onClick={handleGridItemClick}
-                mode="rect"
-                columnNum={2}
-                data={[
-                    {
-                        image: "https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png",
-                        value: "提词器",
-                        link: "/pages/scroll/scroll",
-                    },
-                    {
-                        image: "https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png",
-                        value: "首页",
-                        link: "/pages/index/index",
-                    },
-                ]}
-            />
-        </View>
-    );
+  useLoad(() => {
+    console.log("Page loaded.");
+  });
+  const handleGridItemClick = (item, index) => {
+    console.log(`Clicked on item ${index + 1}: ${item.link}`);
+    // Add your custom logic here
+    Taro.redirectTo({ url: item.link });
+  };
+  return (
+    <View className="index">
+      <CustomHeader title="Home" />
+      <AtGrid
+        onClick={handleGridItemClick}
+        mode="rect"
+        columnNum={2}
+        data={[
+          {
+            image:
+              "https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png",
+            value: "提词器",
+            link: "/pages/scroll/scroll",
+          },
+          {
+            image:
+              "https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png",
+            value: "首页",
+            link: "/pages/index/index",
+          },
+        ]}
+      />
+    </View>
+  );
 }
